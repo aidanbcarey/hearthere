088f38112fc598fe911639ac7b493e3243ee4ba4
aidanbcarey <aidanbcarey@gmail.com>
2021-04-29 18:51:10 -0400

    ugh

--- application.py
+++ application.py
@@ -53,10 +53,10 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 dbs=os.environ['postgres://tggszmgdctisvp:54e275242b7b5e8067a1010e033e9a5428ee589efc8c39844af2c6ed03dd72e2@ec2-23-21-229-200.compute-1.amazonaws.com:5432/df84i2b2pkj62j']
-#print(type(dbs))
+
 # Configure CS50 Library to use SQLite database
 db=psycopg2.connect(dbs, sslmode='require')
-#db = SQL("sqlite:///finance.db")
+
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
     raise RuntimeError("API_KEY not set")
