0d5aaef24c4216281c8297b1cc9988204a19e0ea
aidanbcarey <aidanbcarey@gmail.com>
2021-05-12 02:59:44 -0400

    lyrics

--- utils.py
+++ utils.py
@@ -36,7 +36,8 @@ def get_freq(response,genius,worddata,user):
         
         db.execute("DELETE FROM userfreqs WHERE id = %s",(int(user),))
     for i in ratiot:
-        db.execute("INSERT INTO userfreqs (id, word, freq) VALUES (%s, %s, %s)",(int(user),i[0],i[1]))
+        db.execute("INSERT INTO userfreqs (id, word, freq) VALUES (%s, %s, %s)","hello",4,4))
+    db.execute("INSERT INTO userfreqs (id, word, freq) VALUES (%s, %s, %s)","hello",4,4))
     return(ratiot)
 
 def getlyrics(song,artist,genius):
