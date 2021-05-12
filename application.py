945c7c9185bf131830e75a6ece87d17df3f3506e
aidanbcarey <aidanbcarey@gmail.com>
2021-05-12 02:46:23 -0400

    lyrics

--- application.py
+++ application.py
@@ -312,7 +312,10 @@ def callback():
 @app.route("/deposit", methods=["GET", "POST"])
 @login_required
 def deposit():
-
+    ratiot=[]
+    rows=db.execute("SELECT * FROM userfreqs WHERE id=%s",int(session.get("user_id")))
+    for i in rows:
+        ratiot.append((i["word"],i["freq"])
     return render_template("quoted.html",lyrics=ratiot)
 
 
